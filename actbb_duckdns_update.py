fb3bec9c2ff6a279fcaf5a0a11c062aa763d0b72
sopandian <sopandian@paypal.com>
2016-06-27 14:40:00 +0530

    Utilized OptionParser and given doc

--- actbb_duckdns_update.py
+++ actbb_duckdns_update.py
@@ -1,9 +1,12 @@
+from optparse import OptionParser
 from urllib2 import urlopen
 from HTMLParser import HTMLParser
 
-import sys
 
 class ACTHTMLParser(HTMLParser):
+    """
+    HTML Parser to get IP from portal.actbb.com
+    """
     def __init__(self):
         HTMLParser.__init__(self)
         self._is_p = False
@@ -27,25 +30,58 @@ class ACTHTMLParser(HTMLParser):
 
 
 def main():
-    domain = sys.argv[1]
-    token = sys.argv[2]
+    """
+    Takes domain and token from arg
+    Finds your current IP
+    Updates it to DuckDNS
+    :return: Nothing... None
+    """
+    token = None
+    domain = None
+    parser = OptionParser()
+    parser.add_option('-d', '--domain', dest='domain', help='DuckDNS Domain')
+    parser.add_option('-t', '--token', dest='token', help='DuckDNS Token')
+    (options, args) = parser.parse_args()
+    for opt_key, opt_value in options:
+        if opt_key == 'domain':
+            domain = opt_value
+            continue
+        if opt_key == 'token':
+            token = opt_value
+            continue
+
     ip = find_your_ip()
-    update_your_ip(domain, token, ip)
+    if token and domain and ip:
+        update_your_ip(domain, token, ip)
+    else:
+        print 'Token %s or Domain %s or IP %s is invalid. Not updated in duckDNS' % (token, domain, ip)
     print '#' * 100
 
 
 def find_your_ip():
-    r = urlopen('http://portal.acttv.in/web/chn/home')
+    """
+    Finds IP from portal.acttv.in portal
+    :return: IP
+    """
+    r = urlopen('http://portal.acttv.in/')
     parser = ACTHTMLParser()
     parser.feed(r.read())
     return parser.get_ip()
 
 
 def update_your_ip(domain, token, ip):
+    """
+    Updates IP to DuckDNS
+    :param domain: DuckDNS domain
+    :param token: DuckDNS token
+    :param ip: Your IP address
+    :return: Nothing
+    """
     url = 'https://www.duckdns.org/update?domains=%s&token=%s&ip=%s' % (domain, token, ip)
     print url
     r = urlopen(url)
     print r.read()
+
 
 if __name__ == '__main__':
     main()
